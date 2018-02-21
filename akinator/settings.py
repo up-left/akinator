ce916f8d4583f9d5f34edbec22309d03bc740874
Mike Rodionov <left.right.forward@gmail.com>
2018-02-21 09:15:41 +0700

    Edited ALLOWED_HOSTS

--- akinator/settings.py
+++ akinator/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'zrko%$+@hz_g9ic)choi7506&#g*5@
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['refrig.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
